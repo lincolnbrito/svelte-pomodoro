0e49066f0f1788f08c0672a399cdb8c42ddc7489
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-04-24 13:25:49 -0300

    create Task class

--- src/TaskList.svelte
+++ src/TaskList.svelte
@@ -1,1 +1,22 @@
-<p>I'm a list of tasks</p>
+<script>
+  import { afterUpdate } from 'svelte';
+  import { Task } from './Task.js';
+
+  const tasks = [
+    new Task('plan some fun trip with Ben and Vico'),
+    new Task('buy some flowers to my wife'),
+    new Task('write an article about Svelte')
+  ];
+</script>
+
+<ul>
+  {#each tasks as task, index}
+    <li>{task.description}</li>
+  {/each}
+</ul>
+
+<style>
+  ul {
+    list-style: none;
+  }
+</style>
