fc5913a025a084f720aefa6830f5317f69b18533
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-04-25 01:51:20 -0300

    add State and disable button

--- src/PomodoroTimer.svelte
+++ src/PomodoroTimer.svelte
@@ -5,9 +5,14 @@
 
   // length of a pomodoro in seconds
   const POMODORO_S = minutesToSeconds(25)
+  const LONG_BREAK_S = minutesToSeconds(20)
+  const SHORT_BREAK_S = minutesToSeconds(5)
+  const State = { idle: 'idle', inProgress: 'in progress', resting: 'resting' }
 
-  //time left in the current pomodoro
   let pomodoroTime = POMODORO_S
+  let completedPomodoros = 0
+  let interval
+  let currentState = State.idle
 
   function formatTime(timeInSeconds) {
     const minutes = secondsToMinutes(timeInSeconds)
@@ -16,14 +21,59 @@
     return `${padWithZeroes(minutes)}:${padWithZeroes(remainingSeconds)}`
   }
 
+  function completePomodoro() {
+    clearInterval(interval)
+    completedPomodoros++
+
+    if (completedPomodoros === 4) {
+      rest(LONG_BREAK_S)
+      completedPomodoros = 0
+    } else {
+      rest(SHORT_BREAK_S)
+    }
+  }
+
   function startPomodoro() {
-    setInterval(() => {
+    currentState = State.inProgress
+    interval = setInterval(() => {
+      if (pomodoroTime === 0) {
+        completePomodoro()
+      }
       pomodoroTime -= 1
     }, 1000)
+  }
+
+  function cancelPomodoro() {
+    idle()
+  }
+
+  function rest(time) {
+    currentState = State.resting
+    pomodoroTime = time
+
+    interval = setInterval(() => {
+      if (pomodoroTime === 0) {
+        idle()
+      }
+      pomodoroTime -= 1
+    }, 1000)
+  }
+
+  function idle() {
+    currentState = State.idle
+    clearInterval(interval)
+    pomodoroTime = POMODORO_S
   }
 </script>
 
 <section>
   <p>{formatTime(pomodoroTime)}</p>
-  <button on:click={startPomodoro}>Start</button>
+  <button on:click={startPomodoro} disabled={currentState !== State.idle}>
+    Start
+  </button>
+  <button
+    on:click={cancelPomodoro}
+    disabled={currentState !== State.inProgress}>
+    Cancel
+  </button>
 </section>
